08185b43db6ea5356e521aec26435a19df80e140
Marky Bisarev <bisarmark@gmail.com>
2021-01-17 22:26:59 +0300

    added taiga backe repo

---
+++ NIX/Taiga/repos/taiga-back-master/settings/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014-2017 Andrey Antukh <niwi@niwi.nz>
+# Copyright (C) 2014-2017 Jesús Espino <jespinog@gmail.com>
+# Copyright (C) 2014-2017 David Barragán <bameda@dbarragan.com>
+# Copyright (C) 2014-2017 Alejandro Alonso <alejandro.alonso@kaleidos.net>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os, sys
+
+from .celery import *  # noqa, pylint: disable=unused-wildcard-import
+
+try:
+    print("Trying import local.py settings...", file=sys.stderr)
+    from .local import *  # noqa, pylint: disable=unused-wildcard-import
+except ImportError:
+    print("Trying import development.py settings...", file=sys.stderr)
+    from .development import *  # noqa, pylint: disable=unused-wildcard-import
